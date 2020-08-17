976b9998e0cc61966795df1b9c9fb4b48219f7c6
Darren <darrenmlouw@gmail.com>
2020-08-17 12:02:55 +0200

    Initial Commot

---
+++ question_1.py
@@ -0,0 +1,72 @@
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class PRNG:
+    def __init__(self):
+        self.x = 1
+        self.y = 10000
+        self.z = 3000
+        self.temp = 0
+        self.random = 0
+
+    def randomUniform(self):
+        # X Component
+        self.x = 171*(self.x % 177) - 2 * (self.x/177)
+        if self.x < 0:
+            self.x = self.x + 30269
+        # Y Component
+        self.y = 172 * (self.y % 176) - 35 * (self.y / 176)
+        if self.y < 0:
+            self.y = self.y + 30307
+        # Z Component
+        self.z = 170 * (self.z % 178) - 63 * (self.z / 178)
+        if self.z < 0:
+            self.z = self.z + 30323
+        # Addition of X, Y and Z Variable
+        self.temp = self.x/30269 + self.y/30307 + self.z/30323
+        self.random = self.temp - math.trunc(self.temp)
+
+        return self.random
+
+
+# rand = PRNG()
+# a = np.array([])
+# Max = 10000
+# total = 0
+#
+# for i in range(0, Max):
+#     value = rand.randomUniform()
+#     total += value
+#     TP = np.array([value])
+#     a = np.append(a, TP)
+#
+# print(total/Max)
+# plt.hist(a, bins=40)
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ question_3.py
@@ -0,0 +1,54 @@
+import question_1
+import question_2
+
+uniform = question_1.PRNG()
+
+
+class Modulation:
+    def __init__(self, bits, numM):
+        self.total = bits
+        self.bits = []
+        self.symbols = []
+        self.transmitted = []
+        self.SNR = 0
+        self.Eb = 0
+        self.N0 = 0
+        self.sigma = 0
+        self.M = numM
+
+    def binary(self):
+        temp = uniform.randomUniform()
+        if temp > 0.5:
+            self.bits.append(1)
+        else:
+            self.bits.append(0)
+
+
+BPSK = Modulation(100, 2)
+QAM4 = Modulation(100, 4)
+PSK8 = Modulation(100, 8)
+QAM16 = Modulation(100, 16)
+
+
+def mapBPSK():
+    for i in range(0, BPSK.total):
+        BPSK.binary()
+        length = len(BPSK.bits)
+        if BPSK.bits[length-1] == 1:
+            BPSK.symbols.append(1)
+        else:
+            BPSK.symbols.append(-1)
+
+    print(BPSK.bits)
+    print(BPSK.symbols)
+
+# def map4QAM():
+#
+# def map8PSK():
+#
+# def map16QAM():
+#
+# def addNoise():
+
+mapBPSK()
+
